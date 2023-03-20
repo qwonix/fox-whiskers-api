2b7fa755976db78bb279644873478c6e0c5168be
Roman <roman.qwonix@gmail.com>
2023-03-20 15:47:33 +0300

    init commit

---
+++ src/main/java/ru/qwonix/foxwhiskersapi/FoxWhiskersApplication.java
@@ -0,0 +1,13 @@
+package ru.qwonix.foxwhiskersapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoxWhiskersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoxWhiskersApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/qwonix/foxwhiskersapi/FoxWhiskersApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.qwonix.foxwhiskersapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoxWhiskersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
