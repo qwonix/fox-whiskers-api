04595aae27cfa81815da212923897ff4117a5571
Roman <roman.qwonix@gmail.com>
2023-03-27 16:23:28 +0300

    fix circular dependency

--- src/main/java/ru/qwonix/foxwhiskersapi/config/SecurityConfig.java
+++ src/main/java/ru/qwonix/foxwhiskersapi/config/SecurityConfig.java
@@ -1,21 +1,15 @@
 package ru.qwonix.foxwhiskersapi.config;
 
-import io.jsonwebtoken.security.Keys;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import ru.qwonix.foxwhiskersapi.security.JwtTokenFilter;
 
-import java.nio.charset.StandardCharsets;
-import java.security.Key;
 
 @Configuration
 public class SecurityConfig {
@@ -33,16 +27,6 @@ public class SecurityConfig {
         http.addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder(17);
-    }
-
-    @Bean
-    public Key key(@Value("${jwt.secret}") String secretKey) {
-        return Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8));
     }
 
     @Bean
